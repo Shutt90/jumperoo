197b23b9c64ba0bc1ddfae3a6e2ff989d5ab2017
Liam Pugh <liam.pugh.009@gmail.com>
2022-11-29 19:17:28 +0000

    new image added

--- main.go
+++ main.go
@@ -7,13 +7,24 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
+const (
+	screenWidth  = 800
+	screenHeight = 600
+)
+
 type Game struct{}
 
 var gopher *ebiten.Image
+var grassPlatform *ebiten.Image
 
 func init() {
 	var err error
 	gopher, _, err = ebitenutil.NewImageFromFile("static/gopher.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grassPlatform, _, err = ebitenutil.NewImageFromFile("static/grassplatform.png")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,9 +37,12 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(0.2, 0.2)
+	op.GeoM.Translate(screenWidth/2.25, screenHeight*0.75)
 
-	screen.DrawImage(gopher, nil)
-
+	screen.DrawImage(gopher, op)
+	
+	screen.DrawImage(grassPlatform, nil)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
